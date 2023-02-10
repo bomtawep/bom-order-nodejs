caa6075ee44c557a4c6f5bb445a5e5a8fc6e4df5
bomtawep <bomtawep@gmail.com>
2023-02-10 22:55:36 +0700

    Create Kubernetes for deploy

--- src/controllers/product_type/updateProductType.ts
+++ src/controllers/product_type/updateProductType.ts
@@ -2,12 +2,12 @@ import conPool = require('../../database')
 
 const updateProductTypePool = (request: any, response: any) => {
     const { id } = request.params
-    const { name } = request.body
+    const { name, is_active } = request.body
     const sysDate = new Date();
     if(id == null || id == "") {
         response.status(503).send(`Please put id in your request to update id: $id .`)
     } else {
-        conPool.query('UPDATE bomorder.product_type SET name = ($2), updated = ($3) where id = ($1)', [id, name, sysDate], (error: any, results: any) => {
+        conPool.query('UPDATE bomorder.product_type SET name = ($2), is_active = ($3), updated = ($4) where id = ($1)', [id, name, is_active, sysDate], (error: any, results: any) => {
             if (error) {
                 throw error
             } else {
