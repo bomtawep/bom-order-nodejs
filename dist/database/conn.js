9402ddb6cf857e80bb6b10d5c4444795c2527040
bomtawep <bomtawep@gmail.com>
2023-02-17 07:45:07 +0700

    Edit connection

--- dist/database/conn.js
+++ dist/database/conn.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const connPool = new pg_1.Pool({
-    host: "http://postgresql-bomtawep-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com",
-    user: "postgress",
+    host: "127.0.0.1",
+    user: "postgres",
     database: "bomtawepdb",
     password: "B@13o01m22",
     port: parseInt("5432" || "5432")
