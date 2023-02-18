19291a02e4fe545c9cebb7153b846aac934b7d91
bomtawep <bomtawep@gmail.com>
2023-02-18 11:19:57 +0700

    deploy app

--- dist/database/conn.js
+++ dist/database/conn.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const connPool = new pg_1.Pool({
-    host: "127.0.0.1",
+    host: "localhost",
     user: "postgres",
     database: "bomtawepdb",
     password: "B@13o01m22",
-    port: parseInt("15432" || "15432")
+    port: parseInt("5432" || "5432")
 });
 exports.default = connPool;
--- src/database/conn.ts
+++ src/database/conn.ts
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const connPool = new Pool({
-    host: "127.0.0.1",
+    host: "localhost",
     user: "postgres",
     database: "bomtawepdb",
     password: "B@13o01m22",
-    port: parseInt("15432" || "15432")
+    port: parseInt("5432" || "5432")
   });
   
 export default connPool
