dd6d2ab2ae94c03adda259a5e9749ef203e63880
Taweewat Phimhin <tawep462@orion.postbox.in.th>
2023-03-15 13:50:38 +0700

    Edit  ip postgres

--- src/database/conn.ts
+++ src/database/conn.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const connPool = new Pool({
-    host: "10.130.8.238",
+    host: "10.130.9.179",
     user: "postgres",
     database: "bomtawepdb",
     password: "B@13o01m22",
