17e80f89898fa00d239218daf6f62e32f1c4de42
bomtawep <114811405+bomtawep@users.noreply.github.com>
2023-03-17 12:51:47 +0700

    Update conn.js

--- dist/database/conn.js
+++ dist/database/conn.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const connPool = new pg_1.Pool({
-    host: "10.128.5.24",
+    host: "10.131.4.229",
     user: "postgres",
     database: "bomtawepdb",
     password: "B@13o01m22",
